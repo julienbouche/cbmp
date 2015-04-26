47468f5926c8b541858d6bfa0aaccd98f4afab2d
julienbouche <julien.bouche@gmail.com>
2015-04-26 12:50:25 +0200

    ADD - enhancing form's design

--- addPlaceForm.php
+++ addPlaceForm.php
@@ -1,4 +1,5 @@
 <?php
+require_once('script/db.php');
  /**
   * Form that is displayed when user is adding a place
   */
@@ -8,16 +9,33 @@ if(isset($_GET['lat']) && strlen(trim($_GET['lat'])) > 0 && isset($_GET['lng']) 
 ?>
 <form method='GET' action='ws/addPlace.php' onsubmit='return(sendFormData(this));'>
     <input type='text' placeholder='Name' name='name' />
-    <input type='text' placeholder='Description' name='desc' />
+    <textarea rows=4 placeholder='Description' name='desc'></textarea>
     <input type='text' placeholder='http://' name='website' />
     <input type='text' placeholder='URL Facebook' name='facebook'/>
     <input type='text' placeholder='URL twitter' name='twitter' />
     <input type='hidden' name='lat' value='<?=$lat?>'/>
     <input type='hidden' name='lng' value='<?=$lng?>'/>
-    <select name='type'><option value='1'>Brewery</option><option value='2'>Bar</option></select>
-    <input type='submit' value='OK' />
+    <?php
+    db_connect();
+    $query="SELECT id, name FROM category";
+    
+    $result = mysql_query($query);
+    if($result){
+    ?>
+    <select name='type'>
+        <?php while($row=mysql_fetch_assoc($result)) : ?>
+        <option value='<?=$row['id']?>'><?=$row['name']?></option>
+        <?php endwhile; ?>
+    </select>
+    
+    <?php
+    }    
+    ?>
+    <input type='submit' value='ADD' />
+    
 </form>
 <?php
+    mysql_close();
 }else{
 ?>
 <p>An error occured. Please, try again later</p>
